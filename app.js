b088799ea4f612edaf5f55fe19e5f2fb04c2078d
allahrakha79 <154686133+allahrakha79@users.noreply.github.com>
2024-03-07 21:50:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+//1 Declare a variable called username.
+
+var username; 
+//2 Declare a variable called myName & assign to it a string that represents your Full Name.
+var myName = "ALLAHRAKHA ";
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+
+var message;
+
+// b) Assign “Hello World” to variable message
+
+message = "Hello world";
+
+// c) Display the message in alert box.
+
+window.alert(message);
+
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+var studentName = "ALLAHRAKHA ";
+var studentAge = 44;
+var studentCourse = "Web and Mobile App Development"
+alert(studentName);
+alert(studentAge);
+alert(studentCourse);
+
+//5. Write a script to display the following alert using one JS
+// variable:
+
+alert("PIZZA ")
